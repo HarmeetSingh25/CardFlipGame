56e4551dffa65af50f899850c7ff84d82cea9d38
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-01-30 22:22:01 +0530

    Add cards

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-let arr = ["❤️", "🍏", "🍉", "🎒", "🐶", "🐥", "🦄", "🧠"];
+let arr = ["❤️", "🍏", "🍉", "🎒", "🐶", "🐥", "🦄", "🧠" ,"🍆","🥐"];
 let mainArr = [...arr, ...arr];
-
 let shuffel = mainArr.sort(() => Math.random() - 0.5);
+
 console.log(shuffel);
+
 let CardMainContainer = document.querySelector(".Card-Main-Container");
-let card = document.createElement("div");
-card.classList.add("card");
-let ct = document.createElement("div");
-ct.classList.add("ct");
 
-let cb = document.createElement("div");
-cb.classList.add("flip");
-cb.classList.add("cb");
-CardMainContainer.appendChild(card);
-card.appendChild(ct);
-card.appendChild(cb);
-console.log(CardMainContainer);
+mainArr.forEach(element => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    
+    let ct = document.createElement("div");
+    ct.classList.add("ct");
+    ct.innerHTML=element
+    
+    let cb = document.createElement("div");
+    cb.classList.add("flip");
+    cb.classList.add("cb");
+    
+    card.appendChild(ct);
+    card.appendChild(cb);
+    CardMainContainer.appendChild(card);
+    // console.log(card);
+    
+});
+let card=document.querySelectorAll(".card")
 
+card.forEach(elem=>{
+
+    
+})
+
+
+// console.log(CardMainContainer);
