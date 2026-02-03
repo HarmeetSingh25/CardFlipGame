233dd108928241911dd50003e794524bcea7be56
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-02-03 10:50:54 +0530

    add flip function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let arr = ["❤️", "🍏", "🍉", "🎒", "🐶", "🐥", "🦄", "🧠" ,"🍆","🥐"];
+let arr = ["❤️", "🍏", "🍉", "🎒", "🐶", "🐥", "🦄", "🧠", "🍆", "🥐"];
 let mainArr = [...arr, ...arr];
 let shuffel = mainArr.sort(() => Math.random() - 0.5);
 
@@ -6,30 +6,32 @@ console.log(shuffel);
 
 let CardMainContainer = document.querySelector(".Card-Main-Container");
 
-mainArr.forEach(element => {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    
-    let ct = document.createElement("div");
-    ct.classList.add("ct");
-    ct.innerHTML=element
-    
-    let cb = document.createElement("div");
-    cb.classList.add("flip");
-    cb.classList.add("cb");
-    
-    card.appendChild(ct);
-    card.appendChild(cb);
-    CardMainContainer.appendChild(card);
-    // console.log(card);
-    
+mainArr.forEach((element) => {
+  let id = Math.floor(Math.random() * 100) + 1;
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.id = id;
+
+  let ct = document.createElement("div");
+  ct.classList.add("ct");
+  ct.innerHTML = element;
+
+  let cb = document.createElement("div");
+  cb.classList.add("flip");
+
+  card.appendChild(ct);
+  card.appendChild(cb);
+  CardMainContainer.appendChild(card);
 });
-let card=document.querySelectorAll(".card")
 
-card.forEach(elem=>{
+let cards = document.querySelectorAll(".card");
 
-    
-})
-
-
-// console.log(CardMainContainer);
+cards.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    e.target.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    setTimeout(() => {
+      e.target.style.backgroundColor = "blue";
+    }, 1000);
+  });
+});
